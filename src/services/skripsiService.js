633c1861e1c3b4b488af41ffbf53ebc2abb2ed91
Wahabi86 <gustiabi97@gmail.com>
2025-09-22 01:43:43 +0800

    update url

--- src/services/skripsiService.js
+++ src/services/skripsiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080"; // URL Backend
+const API_URL = import.meta.env.VITE_API_URL; // URL Backend
 
 // GET All Skripsi
 export const getAllSkripsi = async () => {
